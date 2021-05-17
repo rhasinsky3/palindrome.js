24eb59bae48b8bc928c5a3449de299abf283a7c8
Ryan Hasinsky <hasinsky3@gmail.com>
2021-05-16 20:47:57 -0700

    Added some java things

---
+++ touch.js
@@ -0,0 +1,12 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function (){
+  
+  describe("palindrome", function () {
+  
+    it("Return false for non-palindrome", function () {
+      assert(true);
+    })
+  });
+});
