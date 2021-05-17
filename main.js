e379044b5f739b46d56b8980872d8083e7032351
Ryan Hasinsky <hasinsky3@gmail.com>
2021-05-17 12:37:28 -0700

    Uploaded some what of  palindrome page

---
+++ main.js
@@ -0,0 +1,21 @@
+let Phrase = require("<username>-palindrome");
+
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("submit", function() {
+    palindromeTester(event);
+  });
+});
--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,3 @@
-
 module.exports = Phrase;
 
 // Adds `reverse` to all strings.
@@ -12,11 +11,16 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
-  }
+    return this.content;
+  ​}
 
-  // Returns true if the phrase is a palindrome, false otherwise.
-  this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
-  }
+ // Returns the letters in the content.
+ this.letters = function letters() {
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+  ​}
+
+ ​// Returns true if the phrase is a palindrome, false otherwise.
+ ​this.palindrome = function palindrome() {
+   ​return this.processedContent() === this.processedContent().reverse();
+ ​}
 }
